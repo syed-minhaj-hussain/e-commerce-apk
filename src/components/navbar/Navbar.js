1fe84da1a38c79b27d4f3c9eba2fd0f5ab704f1c
SYED MINHAJ HUSSAIN <syed.minhaj0911@gmail.com>
2021-08-06 00:40:36 +0530

    feat

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import navStyle from "./nav.module.css";
-import { useState } from "react";
 import { useAuthContext } from "../../context/AuthContext";
+import { useWishCartContext } from "../../context/WishCartContext";
+import { FaShoppingCart } from "react-icons/fa";
+import { HiOutlineHeart } from "react-icons/hi";
+import { BiShoppingBag } from "react-icons/bi";
+import { ImHome } from "react-icons/im";
 
 export const Navbar = () => {
   const [isActive, setIsActive] = useState(false);
   const { isUserLoggedIn, logout } = useAuthContext();
+  const {
+    state: { cart, wishlist },
+  } = useWishCartContext();
   return (
     <nav className={navStyle.navbar}>
       <div className={navStyle.brandTitle}>Vintage-Mart</div>
@@ -30,13 +38,15 @@ export const Navbar = () => {
               to="/"
               activeStyle={{
                 fontWeight: "bold",
-                color: "white",
+                color: "#333",
+                backgroundColor: "rgb(220,152,20)",
+                height: "100%",
               }}
               className={navStyle.navLinks}
               end
               onClick={() => setIsActive((prev) => !prev)}
             >
-              Home
+              <ImHome style={{ fontSize: "1.25rem" }} />
             </NavLink>
           </li>
           <li>
@@ -44,12 +54,14 @@ export const Navbar = () => {
               to="/products"
               activeStyle={{
                 fontWeight: "bold",
-                color: "white",
+                color: "#333",
+                backgroundColor: "rgb(220,152,20)",
+                height: "100%",
               }}
               className={navStyle.navLinks}
               onClick={() => setIsActive((prev) => !prev)}
             >
-              Products
+              <BiShoppingBag style={{ fontSize: "1.25rem" }} />
             </NavLink>
           </li>
           <li>
@@ -57,12 +69,29 @@ export const Navbar = () => {
               to="/cart"
               activeStyle={{
                 fontWeight: "bold",
-                color: "white",
+                color: "#333",
+                backgroundColor: "rgb(220,152,20)",
+                height: "100%",
               }}
               className={navStyle.navLinks}
+              style={{ position: "relative" }}
               onClick={() => setIsActive((prev) => !prev)}
             >
-              Cart
+              <FaShoppingCart style={{ fontSize: "1.1rem" }} />
+              <p
+                style={{
+                  position: "absolute",
+                  top: "0.1rem",
+                  right: "0.1rem",
+                  padding: "0.2rem 0.4rem",
+                  backgroundColor: "black",
+                  color: "white",
+                  borderRadius: "50%",
+                  fontSize: "0.7rem",
+                }}
+              >
+                {cart.length}
+              </p>
             </NavLink>
           </li>
           <li>
@@ -70,12 +99,29 @@ export const Navbar = () => {
               to="/wishlist"
               activeStyle={{
                 fontWeight: "bold",
-                color: "white",
+                color: "#333",
+                backgroundColor: "rgb(220,152,20)",
+                height: "100%",
               }}
               className={navStyle.navLinks}
+              style={{ position: "relative" }}
               onClick={() => setIsActive((prev) => !prev)}
             >
-              Wishlist
+              <HiOutlineHeart style={{ fontSize: "1.25rem" }} />
+              <p
+                style={{
+                  position: "absolute",
+                  top: "0.1rem",
+                  right: "0.1rem",
+                  padding: "0.2rem 0.4rem",
+                  backgroundColor: "black",
+                  color: "white",
+                  borderRadius: "50%",
+                  fontSize: "0.7rem",
+                }}
+              >
+                {wishlist.length}
+              </p>
             </NavLink>
           </li>
           <li className={`${isUserLoggedIn ? `${navStyle.logout}` : ""} `}>
@@ -94,7 +140,9 @@ export const Navbar = () => {
                 to="/login"
                 activeStyle={{
                   fontWeight: "bold",
-                  color: "white",
+                  color: "#333",
+                  backgroundColor: "rgb(220,152,20)",
+                  height: "100%",
                 }}
                 className={navStyle.navLinks}
                 onClick={() => setIsActive((prev) => !prev)}
